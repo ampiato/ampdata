0611d49b72c9ed276daa2fc562af92b0bd05914b
Tomas Krejci <tomas@krej.ci>
2024-01-23 10:42:59 +0100

    Initial release

---
+++ setup.py
@@ -0,0 +1,36 @@
+# encoding: utf-8
+#
+import os, sys
+from setuptools import setup
+
+setup_requirements = [
+    "setuptools_scm==3.5.0",
+]
+
+install_requires = [
+    "requests >= 2.18",
+    "sseclient-py >= 1.7",
+    "pytz",
+    "pandas >= 0.21",
+    "future >= 0.16",
+]
+if sys.version_info < (3,):
+    install_requires.append("configparser >= 3.5")
+
+setup(
+    name="ampdata",
+    packages=["ampdata"],
+    install_requires=install_requires,
+    tests_require=[
+        "pytest",
+        "pytest-cov >= 2.5",
+        "requests-mock >= 1.3",
+    ],
+    use_scm_version=True,
+    setup_requires=setup_requirements,
+    description="Ampiato AmpData Python library.",
+    author="Ampiato",
+    author_email="info@ampiato.com",
+    url="https://ampiato.com.com",
+    package_data={"ampdata": ["VERSION"]},
+)
